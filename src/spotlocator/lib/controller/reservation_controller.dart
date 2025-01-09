2f1991cd1de94415836a296904f62d879cacf185
Tawaf Mesar <tawafmesar@gmail.com>
2025-01-10 00:19:21 +0300

    feat: Add reservation controller for managing reservation logic

---
+++ src/spotlocator/lib/controller/reservation_controller.dart
@@ -0,0 +1,65 @@
+import 'package:spotlocator/data/datasource/reservation_data.dart';
+import 'package:spotlocator/data/models/reservation_model.dart';
+import 'package:get/get.dart';
+import '../core/class/statusrequest.dart';
+import '../core/functions/handingdatacontroller.dart';
+import '../core/services/services.dart';
+
+abstract class ReservationController extends GetxController {}
+
+class ReservationControllerImp extends ReservationController {
+  MyServices myServices = Get.find();
+
+  String? users_id;
+  StatusRequest statusRequest = StatusRequest.none;
+
+  ReservationData reservationData = ReservationData(Get.find());
+
+  List Reservation = [];
+
+  List<reservation_model> data = [];
+
+  @override
+  void onInit() {
+    users_id = myServices.sharedPreferences.getString("id");
+    getReservationData();
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  getReservationData() async {
+    data.clear();
+    statusRequest = StatusRequest.loading;
+    var response = await reservationData.getdata(users_id!);
+    print("=============================== Controller $response ");
+    statusRequest = handlingData(response);
+    if (StatusRequest.success == statusRequest) {
+      if (response['status'] == "success") {
+        data = (response['data'] as List<dynamic>)
+            .map((e) => reservation_model.fromJson(e as Map<String, dynamic>))
+            .toList();
+        print(data);
+      } else {
+        statusRequest = StatusRequest.failure;
+      }
+    }
+    update();
+  }
+
+  Future<void> _navigateTobackScreen(String title, String middleText) async {
+    Get.defaultDialog(
+      title: title,
+      middleText: middleText,
+    );
+
+    await Future.delayed(Duration(seconds: 3));
+
+    Get.back();
+    Get.back();
+  }
+}
